a97a7189fa77d31a72fb824d338d54dbb2dd99a7
agbenyo <111682210+deborahmawusi6@users.noreply.github.com>
2025-07-14 16:11:24 +0000

    Add .next/static/chunks/src_app_shop_page_tsx_b353971b._.js 1752509482493

---
+++ .next/static/chunks/src_app_shop_page_tsx_b353971b._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_shop_page_tsx_d98db469._.js"
+],
+    source: "dynamic"
+});
